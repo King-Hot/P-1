74f77a5ee4cf47c95a442f8adf7cb7f1f88922d4
SeokJu <asdfg7123@ajou.ac.kr>
2022-01-13 02:34:52 +0900

    spa 구현

---
+++ run_express/public/js/Loginpage.js
@@ -0,0 +1,10 @@
+export default class {
+    constructor() {
+        document.title = "LoginPage";
+    }
+    async getHtml() {
+        return `
+            <h1>This is LoginPage Page</h1>
+        `;
+    }
+}
---
+++ run_express/public/js/freeboard.js
@@ -0,0 +1,10 @@
+export default class {
+    constructor() {
+        document.title = "Free_board";
+    }
+    async getHtml() {
+        return `
+            <h1>This is Free board</h1>
+        `;
+    }
+}
